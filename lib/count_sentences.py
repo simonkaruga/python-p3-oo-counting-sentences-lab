ecae9b3956fa857e5e5081f60cd05a4749630e87
simonkaruga <simonkaruganjoroge@gmail.com>
2025-11-24 14:33:43 +0300

    Add origin

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+            self._value = ""  # or maybe keep old value, but test doesn't set previously
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        if not self._value:
+            return 0
+        sentences = re.split(r'[.!?]', self._value)
+        # Remove empty strings from split
+        sentences = [s.strip() for s in sentences if s.strip()]
+        return len(sentences)
